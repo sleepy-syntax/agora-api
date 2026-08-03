21555668bdd721d1383b00ccd031546d4331080e
SleepySyntax <suyashparate78@gmail.com>
2026-08-03 23:51:46 +0530

    Integrate NestLoggerModule and configure ReqLogMiddleware for request logging in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NestLoggerModule } from './logger';
+import { ReqLogMiddleware } from './middlewares/req-log.middleware';
 
 @Module({
-    imports: [],
+    imports: [NestLoggerModule],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(ReqLogMiddleware).forRoutes('/', '*path');
+    }
+}
