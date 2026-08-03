3b5adc6340a12f64df859c3a62689e33244bd650
SleepySyntax <suyashparate78@gmail.com>
2026-08-03 23:53:11 +0530

    Integrating dotenv for environment variable management, setting a global prefix for routes, and configuring the logger with nestjs-pino

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { config } from 'dotenv';
+config();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from 'nestjs-pino';
+
+const GLOBAL_PREFIX = process.env.GLOBAL_PREFIX ?? 'api/v1';
+const PORT = process.env.PORT ?? 3000;
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule, { bufferLogs: true, autoFlushLogs: true, logger: ['debug'] });
+    app.useLogger(app.get(Logger));
+    app.setGlobalPrefix(GLOBAL_PREFIX);
+    await app.listen(PORT);
+    app.get(Logger).log(`🚀 Server is running on http://localhost:${PORT}/${GLOBAL_PREFIX}`);
 }
 bootstrap();
