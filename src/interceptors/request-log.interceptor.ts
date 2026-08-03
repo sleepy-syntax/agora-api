d018bab4ebb88c4017108cf6d7c9348ab56bc95c
SleepySyntax <suyashparate78@gmail.com>
2026-08-04 01:54:34 +0530

    Add RequestLogInterceptor for detailed HTTP request logging

---
+++ src/interceptors/request-log.interceptor.ts
@@ -0,0 +1,65 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { IncomingHttpHeaders } from 'node:http';
+import { PinoLogger } from 'nestjs-pino';
+import { Observable } from 'rxjs';
+
+interface IRequestLogSource {
+    ip: string;
+    method: string;
+    url: string;
+    hostname: string;
+    protocol: string;
+    body: unknown;
+    query: unknown;
+    params: unknown;
+    headers: IncomingHttpHeaders;
+    routeOptions?: { url?: string };
+}
+
+function getPath(url: string): string {
+    const queryStart = url.indexOf('?');
+    return queryStart === -1 ? url : url.slice(0, queryStart);
+}
+
+function getRelevantHeaders(headers: IncomingHttpHeaders) {
+    return {
+        host: headers.host,
+        contentType: headers['content-type'],
+        contentLength: headers['content-length'],
+        userAgent: headers['user-agent'],
+        referer: headers.referer,
+        forwardedFor: headers['x-forwarded-for'],
+    };
+}
+
+@Injectable()
+export class RequestLogInterceptor implements NestInterceptor {
+    constructor(private readonly logger: PinoLogger) {
+        this.logger.setContext(RequestLogInterceptor.name);
+    }
+
+    intercept(context: ExecutionContext, next: CallHandler): Observable<unknown> {
+        if (context.getType() !== 'http' || !this.logger.logger.isLevelEnabled('debug')) return next.handle();
+
+        const request = context.switchToHttp().getRequest<IRequestLogSource>();
+        const originalUrl = request.url;
+
+        const logRequest = {
+            ip: request.ip,
+            method: request.method,
+            originalUrl,
+            path: getPath(originalUrl),
+            route: request.routeOptions?.url,
+            hostname: request.hostname,
+            protocol: request.protocol,
+            body: request.body,
+            query: request.query,
+            params: request.params,
+            headers: getRelevantHeaders(request.headers),
+        };
+
+        this.logger.debug({ request: logRequest }, 'request received');
+
+        return next.handle();
+    }
+}
