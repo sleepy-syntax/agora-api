4b4f646f74ae265f5e2b2da7c580adc53dbd4705
SleepySyntax <suyashparate78@gmail.com>
2026-08-08 19:32:40 +0530

    Refactor NODE_ENV configuration to include valid options

--- src/configuration.ts
+++ src/configuration.ts
@@ -3,8 +3,11 @@ import { ENVS } from './constants/variables';
 
 export const ConfigurationSchema = Joi.object({
     /* General */
+    [ENVS.NODE_ENV]: Joi.string()
+        .valid('development', 'production', 'test')
+        .default('development')
+        .description('The environment the application is running in'),
     [ENVS.SWAGGER]: Joi.boolean().default(false).description('Enable swagger'),
     [ENVS.GLOBAL_PREFIX]: Joi.string().default('api/v1').description('The global prefix of the application'),
-    [ENVS.NODE_ENV]: Joi.string().default('development').description('The environment the application is running in'),
     [ENVS.PORT]: Joi.number().default(3000).description('The port the application will listen to'),
 });
