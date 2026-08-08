f5178ebcc0276dd88070923eb05467a0a338f066
SleepySyntax <suyashparate78@gmail.com>
2026-08-08 23:27:26 +0530

    Add CLIENT_URL to configuration schema for client URL setup

--- src/configuration.ts
+++ src/configuration.ts
@@ -11,4 +11,5 @@ export const ConfigurationSchema = Joi.object({
     [ENVS.GLOBAL_PREFIX]: Joi.string().default('api/v1').description('The global prefix of the application'),
     [ENVS.PORT]: Joi.number().default(3000).description('The port the application will listen to'),
     [ENVS.LOG_LEVEL]: Joi.string().valid('debug', 'info', 'warn', 'error').default('info').description('The level of logging'),
+    [ENVS.CLIENT_URL]: Joi.string().uri().default('http://localhost:3000').description('The URL of the client'),
 });
