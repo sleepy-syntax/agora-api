14577715af9b19caa53df76bbfaf2d3874d6d5b6
SleepySyntax <suyashparate78@gmail.com>
2026-08-15 17:43:46 +0530

    Refactor e2e tests to use FastifyAdapter for NestJS application

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,26 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
-import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
-describe('AppController (e2e)', () => {
-    let app: INestApplication<App>;
+void describe('AppController (e2e)', function () {
+    let app: NestFastifyApplication;
 
-    beforeEach(async () => {
+    beforeEach(async function () {
         const moduleFixture: TestingModule = await Test.createTestingModule({
             imports: [AppModule],
         }).compile();
 
-        app = moduleFixture.createNestApplication();
+        app = moduleFixture.createNestApplication<NestFastifyApplication>(new FastifyAdapter());
         await app.init();
+        await app.getHttpAdapter().getInstance().ready();
     });
 
-    it('/ (GET)', () => {
-        return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
+    it('/ (GET)', async function () {
+        await request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
     });
 
-    afterEach(async () => {
+    afterEach(async function () {
         await app.close();
     });
 });
