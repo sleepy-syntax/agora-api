85fc2c42b137549d0402603318cc6f91f8ba0315
SleepySyntax <suyashparate78@gmail.com>
2026-09-24 23:11:33 +0530

    Refactor DatabaseModule to remove unnecessary ConfigModule import for MongoDB connection setup

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ENVS } from 'src/constants/variables';
 
 @Module({
     imports: [
         MongooseModule.forRootAsync({
-            imports: [ConfigModule],
             inject: [ConfigService],
             useFactory: (configService: ConfigService) => ({
                 uri: configService.get<string>(ENVS.MONGODB_URI),
